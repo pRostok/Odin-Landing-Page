79dcc6cc7d57d00762c3089dd8488adf9fd4fbfa
pRostok <rostmart29@gmail.com>
2022-09-01 19:36:55 -0700

    refacture code, apply styling

--- script.js
+++ script.js
@@ -2,13 +2,50 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        s2()
+        expandingPan()
         panel.classList.add('active')
     })
 })
 
-function s2() {
+function expandingPan() {
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
+}
+
+const textEl = document.getElementById('catStr')
+const text = textEl.innerHTML
+
+let idx = 1
+let speed = 350 
+
+ticker()
+
+function ticker() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+    setTimeout(ticker, speed)
+}
+
+const ba = document.getElementById('main')
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+let load = 0
+let int = setInterval(blurr, 15)
+
+function blurr() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+
+    ba.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
